2d3c2417c744d25c1263b3fdfb20f561b24c28d3
mehmehmehlol <hkmeganlo@gmail.com>
2021-05-13 00:33:55 -0700

    finished setting up signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import UserDetails from './UserDetails'
+import PersonalDetails from './PersonalDetails'
+import Interest from './Interest'
+import Confirmation from './Confirmation'
 
 export default class Signup extends Component {
 
@@ -6,14 +10,71 @@ export default class Signup extends Component {
     step: 1,
     email: '',
     username: '', 
-    password: ''
+    password: '',
+    firstName: '',
+    lastName: '',
+    country: '',
+    levelOfEducation: '',
+    dateOfBirth: '',
+    interests: ''
+  }
+
+  // go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step - 1 });
+  }
+
+  // proceed to the next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step + 1 });
+  }
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
   }
 
   render() {
-    return (
-      <div>
-        <h1>hello</h1>
-      </div>
-    )
+    const { step } = this.state;
+    const { email, username, password, firstName, lastName, country, levelOfEducation, dateOfBirth, interests } = this.state;
+    const values = { email, username, password, firstName, lastName, country, levelOfEducation, dateOfBirth, interests }
+    
+    switch(step) {
+      case 1: 
+        return (
+          <UserDetails 
+            nextStep={ this.nextStep }
+            handleChange={ this.handleChange }
+            values={ values }
+          />
+        )
+      case 2: 
+        return (
+          <PersonalDetails 
+            prevStep={ this.prevStep }
+            nextStep={ this.nextStep }
+            handleChange={ this.handleChange }
+            values={ values }
+          />
+        )
+      case 3: 
+        return (
+          <Interest 
+            prevStep={ this.prevStep }
+            nextStep={ this.nextStep }
+            handleChange={ this.handleChange }
+          />
+        )
+      case 4: 
+          return (
+            <Confirmation 
+              prevStep={ this.prevStep }
+            />
+          )
+      default: 
+          // do nothing
+    }
   }
 }
